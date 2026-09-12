e85f0360ce750a1b986ab8fb25efb5d3a83df5c0
khzarzisahmed15-cpu <khzarzisahmed15@gmail.com>
2026-09-12 23:14:01 +0600

    Navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import logo from "./assets/logo-text.png";
 function App() {
   return (
     <>
-      <nav>
+      <nav className="flex justify-between items-center container mx-auto my-3">
         <img src={logo} alt="" />
         <div>
-          <ul>
-            <li>
+          <ul className="flex gap-9 font-light">
+            <li className="text-[#DB2777]">
               <a href="">Home</a>
             </li>
             <li>
@@ -23,13 +23,15 @@ function App() {
             </li>
           </ul>
         </div>
-        <div>
+        <div className="flex gap-5 items-center">
           <ul>
             <li>
               <a href="">Sign In</a>
             </li>
           </ul>
-          <button>Sign Up</button>
+          <button className="bg-[#DB2777] text-white py-1 px-4 rounded-2xl">
+            Sign Up
+          </button>
         </div>
       </nav>
     </>
