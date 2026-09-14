6a8512487dd297e1208c6ef3bf12c5ec42cf0f3c
khzarzisahmed15-cpu <khzarzisahmed15@gmail.com>
2026-09-14 18:31:37 +0600

    Hero-Section Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,11 @@
-import logo from "./assets/logo-text.png";
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-      <nav className="flex justify-between items-center container mx-auto my-3">
-        <img src={logo} alt="" />
-        <div>
-          <ul className="flex gap-9 font-light">
-            <li className="text-[#DB2777]">
-              <a href="">Home</a>
-            </li>
-            <li>
-              <a href="">Technologies</a>
-            </li>
-            <li>
-              <a href="">Projects</a>
-            </li>
-            <li>
-              <a href="">About</a>
-            </li>
-            <li>
-              <a href="">Contacts</a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex gap-5 items-center">
-          <ul>
-            <li>
-              <a href="">Sign In</a>
-            </li>
-          </ul>
-          <button className="bg-[#DB2777] text-white py-1 px-4 rounded-2xl">
-            Sign Up
-          </button>
-        </div>
-      </nav>
+      <NavBar></NavBar>
+      <HeroSection></HeroSection>
     </>
   );
 }
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,35 @@
+import bannerStack from "../assets/banner-stack.png";
+// export interface HeroSectionProps {
+//     prop: string
+// }
+
+export default function HeroSection() {
+  return (
+    <>
+      <div className="grid grid-cols-2 justify-center items-center container mx-auto">
+        <div className="space-y-4">
+          <h2 className="text-5xl font-extrabold">
+            Build Your Ideal{" "}
+            <span className="bg-linear-to-r from-orange-600 to-violet-600 bg-clip-text text-transparent">
+              Development Stack
+            </span>
+          </h2>
+          <p className="text-[#475569] text-xl pr-5 my-15">
+            Explore frontend, backend, database, and tooling options, compare
+            them side by side, and put together the stack that fits your next
+            project.
+          </p>
+          <button className="bg-linear-to-r from-orange-600 to-purple-600 text-white text-bold text-xl p-2 rounded-xl hover:scale-105  duration-200 active:scale-100">
+            Explore Technologies
+          </button>
+          <button className="text-bold text-xl p-2 rounded-xl border border-gray-700 text-gray-700 mx-10 px-10 hover:scale-105  duration-200 active:scale-100">
+            Learn More
+          </button>
+        </div>
+        <div>
+          <img src={bannerStack} alt="" className="w-full" />
+        </div>
+      </div>
+    </>
+  );
+}
